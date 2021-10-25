179f71f69534ba61ddbb4b5d1ed24335ba144267
0421mk@naver.com <0421mk@naver.com>
2021-10-25 20:42:38 +0900

    프로그램 구현 시작

--- src/study/ssg/com/Main.java
+++ src/study/ssg/com/Main.java
@@ -1,7 +1,23 @@
 package study.ssg.com;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		
+		System.out.println("==== 프로그램 시작 ====");
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.printf("명령어 입력: ");
+		String command = scanner.nextLine();
+		
+		System.out.println(command);
+		System.out.println("==== 프로그램 끝 ====");
+		
+		// SSG 게시판, 회원 기능이 들어간 프로그램
+		// 게시판은 읽기, 쓰기, 수정, 삭제, 페이징 기능 구현
+		// 회원은 회원가입, 로그인
+		// 리팩토링 => 코드를 잘게 쪼개는, 하나의 파일을 여러개의 메서드화 시키는 작업
+		
 	}
 }
