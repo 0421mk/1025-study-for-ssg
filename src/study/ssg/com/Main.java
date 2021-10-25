8c059151f10113099b252f11bc38f87e345d1b2e
0421mk@naver.com <0421mk@naver.com>
2021-10-25 20:57:13 +0900

    article write making

--- src/study/ssg/com/Main.java
+++ src/study/ssg/com/Main.java
@@ -1,9 +1,13 @@
 package study.ssg.com;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		
+		List<Article> articles = new ArrayList<>();
 		
 		System.out.println("==== 프로그램 시작 ====");
 		Scanner scanner = new Scanner(System.in);
@@ -12,10 +16,21 @@ public class Main {
 			System.out.printf("명령어 입력: ");
 			String command = scanner.nextLine();
 			
-			if(command.equals("article list")) {
+			if(command.equals("article write")) {
+				
+				System.out.printf("제목: ");
+				String title = scanner.nextLine();
+				
+				System.out.printf("내용: ");
+				String body = scanner.nextLine();
+				
+				Article article = new Article(title, body);
+				articles.add(article);
+				
+				System.out.println(article.articleId + "번 게시물이 생성되었습니다.");
+				
+			} else if(command.equals("article list")) {
 				System.out.println("article list");
-			} else if(command.equals("article write")) {
-				System.out.println("article write");
 			} else if(command.equals("article modify")) {
 				System.out.println("article modify");
 			} else if(command.equals("article delete")) {
@@ -30,5 +45,22 @@ public class Main {
 		
 		System.out.println("==== 프로그램 끝 ====");
 		
+	}
+}
+
+class Article {
+	// static 힙 메모리로 들어가면서
+	// 처음 선언된 딱 한번만 초기화가 된다.
+	// 그 뒤로는 값이 누적
+	static int index = 0;
+	int articleId;
+	String title;
+	String body;
+	
+	Article(String title, String body) {
+		this.title = title;
+		this.body = body;
+		this.index++;
+		this.articleId = this.index;
 	}
 }
