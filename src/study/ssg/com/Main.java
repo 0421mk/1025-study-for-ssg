bdde7f93a0be0a482cfa0e33260fa4afb1ff6aa5
0421mk@naver.com <0421mk@naver.com>
2021-10-25 22:05:46 +0900

    article modify making

--- src/study/ssg/com/Main.java
+++ src/study/ssg/com/Main.java
@@ -30,51 +30,99 @@ public class Main {
 				System.out.println(article.articleId + "번 게시물이 생성되었습니다.");
 
 			} else if (command.equals("article list")) {
-				
-				if(articles.size() == 0) {
+
+				if (articles.size() == 0) {
 					System.out.println("게시물이 존재하지 않습니다.");
 					continue;
 				}
-				
+
 				System.out.printf("  번호 /    제목\n");
 				for (Article article : articles) {
 					System.out.printf("%4d / %10s\n", article.articleId, article.title);
 				}
 
 			} else if (command.startsWith("article detail ")) {
-				
-				command = command.substring("article detail ".length());
-				
-				if(command.equals("")) {
+
+				String input = command.substring("article detail ".length()).trim();
+
+				if (input.equals("")) {
 					System.out.println("숫자를 입력해주세요.");
 					continue;
 				}
-				
+
 				// 정수인지 확인하는 정규표현식으로 문자열 검토
-				boolean cmdCheck = command.matches("[+-]?\\d*(\\.\\d+)?");
-				
+				boolean cmdCheck = input.matches("-?\\d+");
+
 				int foundId = 0;
-				if(cmdCheck) {
-					foundId = Integer.parseInt(command);
+				if (cmdCheck) {
+					foundId = Integer.parseInt(input);
 				} else {
 					System.out.println("숫자를 입력해주세요.");
 					continue;
 				}
-								
-				for(Article article : articles) {
-					if(article.articleId == foundId) {
-						System.out.printf("번호: %d\n", article.articleId);
-						System.out.printf("제목: %s\n", article.title);
-						System.out.printf("내용: %s\n", article.body);
-						System.out.println("===========");
-					} else {
-						System.out.println(foundId + "번 게시물이 존재하지 않습니다.");
-						continue;
+
+				Article foundArticle = null;
+
+				for (Article article : articles) {
+					if (article.articleId == foundId) {
+						foundArticle = article;
 					}
 				}
-								
-			} else if (command.equals("article modify")) {
-				System.out.println("article modify");
+
+				if (foundArticle != null) {
+					System.out.printf("번호: %d\n", foundArticle.articleId);
+					System.out.printf("제목: %s\n", foundArticle.title);
+					System.out.printf("내용: %s\n", foundArticle.body);
+					System.out.println("===========");
+				} else {
+					System.out.println(foundId + "번 게시물이 존재하지 않습니다.");
+					continue;
+				}
+
+			} else if (command.startsWith("article modify ")) {
+
+				String input = command.substring("article modify ".length()).trim();
+
+				if (input.equals("")) {
+					System.out.println("숫자를 입력해주세요.");
+					continue;
+				}
+
+				// 정수인지 확인하는 정규표현식으로 문자열 검토
+				boolean cmdCheck = input.matches("-?\\d+");
+
+				int foundId = 0;
+				if (cmdCheck) {
+					foundId = Integer.parseInt(input);
+				} else {
+					System.out.println("숫자를 입력해주세요.");
+					continue;
+				}
+
+				Article foundArticle = null;
+
+				for (Article article : articles) {
+					if (article.articleId == foundId) {
+						foundArticle = article;
+					}
+				}
+				
+				if (foundArticle != null) {
+					System.out.printf("제목: ");
+					String title = scanner.nextLine();
+
+					System.out.printf("내용: ");
+					String body = scanner.nextLine();
+
+					foundArticle.title = title;
+					foundArticle.body = body;
+
+					System.out.println(foundArticle.articleId + "번 게시물이 수정되었습니다.");
+				} else {
+					System.out.println(foundId + "번 게시물이 존재하지 않습니다.");
+					continue;
+				}
+
 			} else if (command.equals("article delete")) {
 				System.out.println("article delete");
 			} else if (command.equals("system exit")) {
