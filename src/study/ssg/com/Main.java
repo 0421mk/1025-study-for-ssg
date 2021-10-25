bbe3a9dbe614b77f1326f8d0b25d277bb870dcc2
0421mk@naver.com <0421mk@naver.com>
2021-10-25 21:18:40 +0900

    article list making

--- src/study/ssg/com/Main.java
+++ src/study/ssg/com/Main.java
@@ -6,45 +6,55 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
 		List<Article> articles = new ArrayList<>();
-		
+
 		System.out.println("==== 프로그램 시작 ====");
 		Scanner scanner = new Scanner(System.in);
-		
-		while(true) {
+
+		while (true) {
 			System.out.printf("명령어 입력: ");
 			String command = scanner.nextLine();
-			
-			if(command.equals("article write")) {
-				
+
+			if (command.equals("article write")) {
+
 				System.out.printf("제목: ");
 				String title = scanner.nextLine();
-				
+
 				System.out.printf("내용: ");
 				String body = scanner.nextLine();
-				
+
 				Article article = new Article(title, body);
 				articles.add(article);
-				
+
 				System.out.println(article.articleId + "번 게시물이 생성되었습니다.");
+
+			} else if (command.equals("article list")) {
 				
-			} else if(command.equals("article list")) {
-				System.out.println("article list");
-			} else if(command.equals("article modify")) {
+				if(articles.size() == 0) {
+					System.out.println("게시물이 존재하지 않습니다.");
+					continue;
+				}
+				
+				System.out.printf("  번호 /    제목\n");
+				for (Article article : articles) {
+					System.out.printf("%4d / %10s\n", article.articleId, article.title);
+				}
+
+			} else if (command.equals("article modify")) {
 				System.out.println("article modify");
-			} else if(command.equals("article delete")) {
+			} else if (command.equals("article delete")) {
 				System.out.println("article delete");
-			} else if(command.equals("system exit")) {
+			} else if (command.equals("system exit")) {
 				System.out.println("system exit");
 				break;
 			} else {
 				System.out.println("잘못된 명령어입니다.");
 			}
 		}
-		
+
 		System.out.println("==== 프로그램 끝 ====");
-		
+
 	}
 }
 
@@ -56,7 +66,7 @@ class Article {
 	int articleId;
 	String title;
 	String body;
-	
+
 	Article(String title, String body) {
 		this.title = title;
 		this.body = body;
