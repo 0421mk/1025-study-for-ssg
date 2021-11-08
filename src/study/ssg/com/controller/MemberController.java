98f35e899f937f798594f70b9187d09dbc4428a3
0421mk@naver.com <0421mk@naver.com>
2021-11-08 22:09:42 +0900

    로그아웃 기능 구현, 로그인 여부 체크

--- src/study/ssg/com/controller/MemberController.java
+++ src/study/ssg/com/controller/MemberController.java
@@ -11,7 +11,7 @@ public class MemberController extends Controller {
 	public MemberController(Scanner scanner) {
 		this.scanner = scanner;
 	}
-	
+
 	public void doAction(String command, String actionMethodName) {
 		switch (actionMethodName) {
 		case "join":
@@ -20,8 +20,16 @@ public class MemberController extends Controller {
 		case "login":
 			doLogin();
 			break;
+		case "logout":
+			doLogout();
+			break;
 		case "loginCheck":
-			System.out.println(loginedMember.name);
+			try {
+				System.out.println(loginedMember.name);
+			} catch (NullPointerException e) {
+				System.out.println("현재 로그인 상태가 아닙니다.");
+			}
+			
 			break;
 		default:
 			System.out.println("존재하지 않는 메서드입니다.");
@@ -29,22 +37,37 @@ public class MemberController extends Controller {
 		}
 	}
 
+	private void doLogout() {
+		if (loginedMember == null) {
+			System.out.println("로그인 후 이용해주세요.");
+			return;
+		}
+
+		loginedMember = null;
+		System.out.println("로그아웃 하였습니다.");
+	}
+
 	private void doLogin() {
-		
+		// Spring Interceptor
+		if (loginedMember != null) {
+			System.out.println("로그아웃 후 이용해주세요.");
+			return;
+		}
+
 		Member foundMember = null;
-		
-		if(members.size() == 0) {
+
+		if (members.size() == 0) {
 			System.out.println("회원이 없습니다.");
 			return;
 		}
-		
+
 		String loginId = null;
-		
+
 		while (true) {
 			System.out.printf("로그인 ID: ");
 			loginId = scanner.nextLine();
 			boolean passCheck = false;
-			
+
 			for (Member member : members) {
 				if (member.loginId.equals(loginId)) {
 					passCheck = true;
@@ -52,22 +75,22 @@ public class MemberController extends Controller {
 					break;
 				}
 			}
-			
-			if(passCheck) {
+
+			if (passCheck) {
 				break;
 			} else {
 				System.out.println("존재하지 않는 아이디입니다.");
 				continue;
 			}
 		}
-		
+
 		String loginPw = null;
-		
+
 		while (true) {
 			System.out.printf("로그인 PW: ");
 			loginPw = scanner.nextLine();
 			boolean passCheck = false;
-			
+
 			if (foundMember.loginPw.equals(loginPw)) {
 				// 로그인 성공
 				break;
@@ -76,13 +99,17 @@ public class MemberController extends Controller {
 				continue;
 			}
 		}
-		
+
 		System.out.println(foundMember.name + "님이 로그인하셨습니다.");
 		loginedMember = foundMember;
-		
+
 	}
 
 	public void doJoin() {
+		if (loginedMember != null) {
+			System.out.println("로그아웃 후 이용해주세요.");
+			return;
+		}
 
 		String loginId;
 		String loginPw;
